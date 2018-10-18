b15bd290fce8da88ed5e43a54614899d0869501b
LotusTerHaar <lotus.ter.haar@gmail.com>
2018-10-18 17:25:59 +0200

    data van de Library en playlists is verplaatst naar de second layer

--- src/main/java/oose/dea/lotusterhaar/controller/PlaylistController.java
+++ src/main/java/oose/dea/lotusterhaar/controller/PlaylistController.java
@@ -1,6 +1,6 @@
 package oose.dea.lotusterhaar.controller;
 
-import oose.dea.lotusterhaar.domain.*;
+import oose.dea.lotusterhaar.domain.Library;
 import oose.dea.lotusterhaar.services.PlaylistRestService;
 
 import javax.inject.Inject;
@@ -9,8 +9,7 @@ import javax.inject.Singleton;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Singleton
 @Path("/playlists")
@@ -18,32 +17,11 @@ public class PlaylistController {
     @Inject
     @Named("playlistRestService")
     private PlaylistRestService playlistService = new PlaylistRestService();
-    private List<Playlist> playlists = new ArrayList();
-    private List<Track> tracklist1 = new ArrayList<>();
-    private List<Track> tracklist2 = new ArrayList<>();
-    private Library library;
-    private Track track1, track2, track3, track4, track5;
-
-    public PlaylistController() {
-        track1 = new SongTrack(1, "Song for Someone", "The Frames", 350, 0, false, "The cost");
-        track2 = new VideoTrack(2, "The cost", "The Frames", 423, 37, true, "10-01-2005", "Title Song from the Album The Cost");
-        track3 = new VideoTrack(3, "Falling Slowly", "The Frames", 436, 10, true, "10-01-2005", "Title Song from the Album The Cost");
-        track4 = new SongTrack(4, "Love Story", "Taylor Swift", 380, 100, false, "Fearless");
-        track5 = new SongTrack(5, "Girls Like You", "Maroon 5", 400, 0, true, "Red Pill Blues");
-        tracklist1.add(track1);
-        tracklist1.add(track2);
-        tracklist1.add(track3);
-        tracklist2.add(track4);
-        tracklist2.add(track5);
-        playlists.add(new Playlist(1, "Death metal", true, tracklist1));
-        playlists.add(new Playlist(2, "Pop", true, tracklist2));
-        library = new Library(playlists);
-    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Library getPlaylists() {
-        library = new Library(playlists);
+        Library library = playlistService.createLibrary();
         return library;
     }
 
@@ -53,7 +31,7 @@ public class PlaylistController {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllTracksFromPlaylist(@PathParam("id") final int id, @QueryParam("token") String token) throws Exception {
         try {
-            return Response.ok().entity(playlistService.getAllTracksFromPlaylist(id, token, library)).build();
+            return Response.ok().entity(playlistService.getAllTracksFromPlaylist(id, token)).build();
         } catch (Exception e) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
--- src/main/java/oose/dea/lotusterhaar/services/PlaylistRestService.java
+++ src/main/java/oose/dea/lotusterhaar/services/PlaylistRestService.java
@@ -1,20 +1,46 @@
 package oose.dea.lotusterhaar.services;
 
-import oose.dea.lotusterhaar.domain.Library;
-import oose.dea.lotusterhaar.domain.Playlist;
-import oose.dea.lotusterhaar.domain.TrackOverview;
+import oose.dea.lotusterhaar.domain.*;
 
 import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
 
 @Named("playlistRestService")
 public class PlaylistRestService {
 
-    public TrackOverview getAllTracksFromPlaylist(int id, String token, Library library) throws Exception {
+    private List<Playlist> playlists = new ArrayList();
+    private List<Track> tracklist1 = new ArrayList<>();
+    private List<Track> tracklist2 = new ArrayList<>();
+    private Library library;
+    private Track track1, track2, track3, track4, track5;
+
+    public PlaylistRestService() {
+        track1 = new SongTrack(1, "Song for Someone", "The Frames", 350, 0, false, "The cost");
+        track2 = new VideoTrack(2, "The cost", "The Frames", 423, 37, true, "10-01-2005", "Title Song from the Album The Cost");
+        track3 = new VideoTrack(3, "Falling Slowly", "The Frames", 436, 10, true, "10-01-2005", "Title Song from the Album The Cost");
+        track4 = new SongTrack(4, "Love Story", "Taylor Swift", 380, 100, false, "Fearless");
+        track5 = new SongTrack(5, "Girls Like You", "Maroon 5", 400, 0, true, "Red Pill Blues");
+        tracklist1.add(track1);
+        tracklist1.add(track2);
+        tracklist1.add(track3);
+        tracklist2.add(track4);
+        tracklist2.add(track5);
+        playlists.add(new Playlist(1, "Death metal", true, tracklist1));
+        playlists.add(new Playlist(2, "Pop", true, tracklist2));
+        library = new Library(playlists);
+    }
+
+    public TrackOverview getAllTracksFromPlaylist(int id, String token) throws Exception {
         for (Playlist playlist : library.getPlaylists()) {
             if (token.equals("1234-1234-1234-1234") && id == playlist.getId()) {
                 return new TrackOverview(playlist.getTracks());
             }
         }
         throw new Exception("No tracks found!");
+    }
+
+    public Library createLibrary() {
+        return library;
     }
 }

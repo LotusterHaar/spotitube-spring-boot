608cc56559b555a1341a0374b09742c9328935c7
LotusTerHaar <lotus.ter.haar@gmail.com>
2018-10-25 15:50:55 +0200

    Database aangepast, geen song_video_id meer, id Songtrack/id Videotrack verwijst naar id Track

--- database/songtrack v1.sql
+++ database/songtrack v1.sql
@@ -5,7 +5,8 @@ LIMIT 0, 1000
 -- Date: 2018-10-22 18:54
 */
 INSERT INTO `songtrack` (`id`,`album`) VALUES (1,'%');
-INSERT INTO `songtrack` (`id`,`album`) VALUES (2,'X');
-INSERT INTO `songtrack` (`id`,`album`) VALUES (3,'+');
-INSERT INTO `songtrack` (`id`,`album`) VALUES (7,'Expectations');
-INSERT INTO `songtrack` (`id`,`album`) VALUES (8,'Florida Georgia Line');
+INSERT INTO `songtrack` (`id`,`album`) VALUES (2,'%');
+INSERT INTO `songtrack` (`id`,`album`) VALUES (3,'X');
+INSERT INTO `songtrack` (`id`,`album`) VALUES (4,'+');
+INSERT INTO `songtrack` (`id`,`album`) VALUES (8,'Expectations');
+INSERT INTO `songtrack` (`id`,`album`) VALUES (9,'Florida Georgia Line');
--- database/videotrack v1.sql
+++ database/videotrack v1.sql
@@ -4,6 +4,6 @@ LIMIT 0, 1000
 
 -- Date: 2018-10-22 18:53
 */
-INSERT INTO `videotrack` (`id`,`publicationDate`,`description`) VALUES (4,'2018-05-30','https://youtu.be/aJOTlE1K90k');
-INSERT INTO `videotrack` (`id`,`publicationDate`,`description`) VALUES (5,'2011-08-12','https://youtu.be/T3E9Wjbq44E');
-INSERT INTO `videotrack` (`id`,`publicationDate`,`description`) VALUES (6,'2018-06-09','https://youtu.be/l25AL0BdD6w');
+INSERT INTO `videotrack` (`id`,`publicationDate`,`description`) VALUES (5,'2018-05-30','https://youtu.be/aJOTlE1K90k');
+INSERT INTO `videotrack` (`id`,`publicationDate`,`description`) VALUES (6,'2011-08-12','https://youtu.be/T3E9Wjbq44E');
+INSERT INTO `videotrack` (`id`,`publicationDate`,`description`) VALUES (7,'2018-06-09','https://youtu.be/l25AL0BdD6w');
